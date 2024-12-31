c3c7a33b69357bb1ca8650ec6b02bf0317ad41fb
Pramod Lawate <pramod.lawate@FujitsuMicrosoftPracticeUK.onmicrosoft.com>
2024-12-31 21:54:36 +0000

    Add basket API features and Docker configurations
    Updated Basket.API.csproj to include new package references for health checks, gRPC, Marten, and other libraries. Added project reference to BuildingBlocks.csproj.
    
    Created new namespaces for basket operations: CheckoutBasket, DeleteBasket, GetBasket, and StoreBasket, each with corresponding endpoints and handlers.
    
    Added BasketRepository and CachedBasketRepository for data operations using Marten and IDistributedCache, respectively. Defined IBasketRepository interface.
    
    Updated Dockerfile to reference BuildingBlocks.csproj and expose port 8081. Updated docker-compose files to configure services for basketdb, distributedcache, and basket.api.
    
    Added BasketNotFoundException, ShoppingCart, and ShoppingCartItem classes. Updated Program.cs to configure services including Marten, Redis caching, health checks, and exception handling.
    
    Updated appsettings.json with connection strings for basket database and Redis. Updated launchSettings.json for Docker settings and launch URL. Updated docker-compose.dcproj to change Docker launch action and service name.

---
+++ src/eShopsMicroservices/Services/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
@@ -0,0 +1,19 @@
+﻿using Basket.API.Data;
+using Basket.API.Models;
+using BuildingBlocks.CQRS;
+
+namespace Basket.API.Basket.CheckoutBasket
+{
+    public record CheckoutBasketCommand(ShoppingCart shopingcart) : ICommand<CheckoutBasketResult>;
+
+    public record CheckoutBasketResult(bool IsSuccess);
+
+
+    public class CheckoutBasketCommandHandler(IBasketRepository repository) : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+    {
+        public Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new CheckoutBasketResult(true));
+        }
+    }
+}
--- src/eShopsMicroservices/Services/Basket.API/Program.cs
+++ src/eShopsMicroservices/Services/Basket.API/Program.cs
@@ -1,34 +1,57 @@
+using Basket.API.Data;
+using Basket.API.Models;
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+using Carter;
+using HealthChecks.UI.Client;
+using Marten;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+//Data Services
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("BasketConnection")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("RedisConnection");
+    //options.InstanceName = "Basket";
+});
+
+//Cross-Cutting Services
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("BasketConnection")!)
+    .AddRedis(builder.Configuration.GetConnectionString("RedisConnection")!);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
